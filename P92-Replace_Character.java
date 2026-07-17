ba4ef994ee27744d615e8be5857e411287db03c5
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:32:12 +0530

    Add ReplaceCharacter class to replace 'a' with '@'

---
+++ P92-Replace_Character.java
@@ -0,0 +1,7 @@
+public class ReplaceCharacter {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+
+        System.out.println(text.replace('a', '@'));
+    }
+}
