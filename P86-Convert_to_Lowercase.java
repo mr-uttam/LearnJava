17068c8ddefc65cd7072e95fb23dbf7916ab5e3b
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:22:01 +0530

    Create P86-Convert_to_Lowercase.java

---
+++ P86-Convert_to_Lowercase.java
@@ -0,0 +1,7 @@
+public class ConvertToLowercase {
+    public static void main(String[] args) {
+        String text = "JAVA PROGRAMMING";
+
+        System.out.println(text.toLowerCase());
+    }
+}
