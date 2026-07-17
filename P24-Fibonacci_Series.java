bf7802fc6acf9eb98a93a14ce2b4ba2b3f4fef0c
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 11:47:56 +0530

    Implement Fibonacci series in Java

---
+++ P24-Fibonacci_Series.java
@@ -0,0 +1,14 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10;
+        int first = 0;
+        int second = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
