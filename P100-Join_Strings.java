cd03637fa950125db17d0a6066916fdd431f6267
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:36:28 +0530

    Add JoinStrings class to join and print strings

---
+++ P100-Join_Strings.java
@@ -0,0 +1,7 @@
+public class JoinStrings {
+    public static void main(String[] args) {
+        String result = String.join(" ", "Welcome", "to", "Java");
+
+        System.out.println(result);
+    }
+}
