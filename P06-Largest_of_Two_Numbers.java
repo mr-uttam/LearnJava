988bf2b28f504be07b554e268c8341eab8ad391e
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 11:32:36 +0530

    Add LargestofTwo_Numbers.java to compare two integers

---
+++ P06-Largest_of_Two_Numbers.java
@@ -0,0 +1,12 @@
+public class LargestofTwo_Numbers {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 30;
+
+        if (a > b) {
+            System.out.println(a + " is greater");
+        } else {
+            System.out.println(b + " is greater");
+        }
+    }
+}
