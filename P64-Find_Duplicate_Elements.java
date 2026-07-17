007657646857dd6916f3fdd394bfb05c7e9d6431
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:08:12 +0530

    Add FindDuplicateElements class to identify duplicates

---
+++ P64-Find_Duplicate_Elements.java
@@ -0,0 +1,13 @@
+public class FindDuplicateElements {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 20, 40, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    System.out.println(numbers[i]);
+                }
+            }
+        }
+    }
+}
