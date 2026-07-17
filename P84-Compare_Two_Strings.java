3f812f90b0acad31236445516c68752ca4ada44f
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:21:07 +0530

    Create P84-Compare_Two_Strings.java

---
+++ P84-Compare_Two_Strings.java
@@ -0,0 +1,12 @@
+public class CompareTwoStrings {
+    public static void main(String[] args) {
+        String first = "Java";
+        String second = "Java";
+
+        if (first.equals(second)) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are Not Equal");
+        }
+    }
+}
