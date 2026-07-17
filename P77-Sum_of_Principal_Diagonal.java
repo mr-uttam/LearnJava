23762b0ab4ef4b3f174f76103727e4a008f6b78e
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:17:37 +0530

    Implement sum of principal diagonal in a matrix

---
+++ P77-Sum_of_Principal_Diagonal.java
@@ -0,0 +1,17 @@
+public class SumOfPrincipalDiagonal {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
