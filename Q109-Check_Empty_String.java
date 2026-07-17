68cb8dcc526d0b1a743d93bee69efc5d1ea499d4
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 14:17:35 +0530

    Create Q109-Check_Empty_String.java

---
+++ Q109-Check_Empty_String.java
@@ -0,0 +1,11 @@
+public class CheckEmptyString {
+    public static void main(String[] args) {
+        String text = "";
+
+        if (text.isEmpty()) {
+            System.out.println("String is Empty");
+        } else {
+            System.out.println("String is Not Empty");
+        }
+    }
+}
