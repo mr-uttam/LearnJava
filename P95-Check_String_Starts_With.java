e43be71b105c6a6ee00587e446fd30e4555bc222
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:33:30 +0530

    Add CheckStringStartsWith class to check string prefix

---
+++ P95-Check_String_Starts_With.java
@@ -0,0 +1,7 @@
+public class CheckStringStartsWith {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+
+        System.out.println(text.startsWith("Java"));
+    }
+}
