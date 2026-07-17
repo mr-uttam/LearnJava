b037d0d9ac913f90abd952f1a053781cc888adc5
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:31:46 +0530

    Add RemoveWhiteSpaces class to eliminate white spaces

---
+++ P91-Remove_White_Spaces.java
@@ -0,0 +1,7 @@
+public class RemoveWhiteSpaces {
+    public static void main(String[] args) {
+        String text = "Java Programming Language";
+
+        System.out.println(text.replace(" ", ""));
+    }
+}
