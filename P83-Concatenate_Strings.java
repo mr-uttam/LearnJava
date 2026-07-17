64d13cceee91fc76ed761b9f67f6bff627e2304f
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:20:42 +0530

    Add ConcatenateStrings class to concatenate two strings

---
+++ P83-Concatenate_Strings.java
@@ -0,0 +1,10 @@
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = "Java";
+
+        String result = first + " " + second;
+
+        System.out.println(result);
+    }
+}
