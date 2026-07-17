27dfe82a2084d8e79c1924c2a8bd798bdb8e6b58
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:33:57 +0530

    Add CheckStringEndsWith class to check string endings

---
+++ P96-Check_String_Ends_With.java
@@ -0,0 +1,7 @@
+public class CheckStringEndsWith {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+
+        System.out.println(text.endsWith("ing"));
+    }
+}
