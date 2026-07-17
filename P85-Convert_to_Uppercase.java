1505570ee390fee2c24ae3f16ec0803ac0227322
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:21:35 +0530

    Add ConvertToUppercase class to convert text to uppercase

---
+++ P85-Convert_to_Uppercase.java
@@ -0,0 +1,7 @@
+public class ConvertToUppercase {
+    public static void main(String[] args) {
+        String text = "java programming";
+
+        System.out.println(text.toUpperCase());
+    }
+}
