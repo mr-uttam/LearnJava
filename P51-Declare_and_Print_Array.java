3ad756613feccca32691b57074893df606846794
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:02:08 +0530

    Add array declaration and printing functionality

---
+++ P51-Declare_and_Print_Array.java
@@ -0,0 +1,9 @@
+public class DeclareAndPrintArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
