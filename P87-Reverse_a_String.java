dc2958197b1ef57282d4d6b476665dd25b4a5243
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:22:24 +0530

    Create P87-Reverse_a_String.java

---
+++ P87-Reverse_a_String.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String text = "Java";
+        String reverse = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverse += text.charAt(i);
+        }
+
+        System.out.println(reverse);
+    }
+}
