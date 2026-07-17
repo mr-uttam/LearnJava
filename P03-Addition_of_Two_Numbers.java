337bcf24b3a8b8de198afcb0c358f508a0f65303
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 11:28:15 +0530

    Create P03-Addition_of_Two_Numbers.java

---
+++ P03-Addition_of_Two_Numbers.java
@@ -0,0 +1,8 @@
+public class P03_Addition_of_Two_Numbers {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
