1b705d1fcfcad244aec45f872d91f02e93a7fd18
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 14:14:22 +0530

    Implement duplicate character finder in Java

---
+++ Q103-Find_Duplicate_Characters.java
@@ -0,0 +1,32 @@
+public class FindDuplicateCharacters {
+    public static void main(String[] args) {
+        String text = "programming";
+
+        for (int i = 0; i < text.length(); i++) {
+            int count = 1;
+
+            if (text.charAt(i) == ' ') {
+                continue;
+            }
+
+            for (int j = i + 1; j < text.length(); j++) {
+                if (text.charAt(i) == text.charAt(j)) {
+                    count++;
+                }
+            }
+
+            boolean printed = false;
+
+            for (int k = 0; k < i; k++) {
+                if (text.charAt(i) == text.charAt(k)) {
+                    printed = true;
+                    break;
+                }
+            }
+
+            if (count > 1 && !printed) {
+                System.out.println(text.charAt(i));
+            }
+        }
+    }
+}
