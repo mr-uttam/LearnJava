b4fcce3f3da402fcd39058f57cdcf76dceb0277e
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 12:34:32 +0530

    Add CheckStringContains class to check substring

---
+++ P97-Check_String_Contains.java
@@ -0,0 +1,7 @@
+public class CheckStringContains {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+
+        System.out.println(text.contains("Program"));
+    }
+}
