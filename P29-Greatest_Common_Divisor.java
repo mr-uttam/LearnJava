63246783fb4f028e1eca46f8c2aa0f1ba91283c6
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 11:50:13 +0530

    Implement GCD calculation in GreatestCommonDivisor

---
+++ P29-Greatest_Common_Divisor.java
@@ -0,0 +1,14 @@
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        int a = 48;
+        int b = 18;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD = " + a);
+    }
+}
