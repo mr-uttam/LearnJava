6f7448827f45afb4a9e685b315b9518c5ffd7f05
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 14:22:43 +0530

    Create Q116-StringBuffer_Example.java

---
+++ Q116-StringBuffer_Example.java
@@ -0,0 +1,9 @@
+public class StringBufferExample {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer("Java");
+
+        buffer.append(" Programming");
+
+        System.out.println(buffer);
+    }
+}
