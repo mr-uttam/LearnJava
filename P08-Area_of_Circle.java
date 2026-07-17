6d1174d2413182ff7a9968a64a9158b96f1d5c0a
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-17 11:34:05 +0530

    Add Area of Circle calculation program

---
+++ P08-Area_of_Circle.java
@@ -0,0 +1,8 @@
+public class AreaofCircle {
+    public static void main(String[] args) {
+        double radius = 7;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area = " + area);
+    }
+}
