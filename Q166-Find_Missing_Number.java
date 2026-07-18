24baf5cdb5367d94c7fb225c64e1684bee255642
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-18 09:51:30 +0530

    Implement missing number finder in Java

---
+++ Q166-Find_Missing_Number.java
@@ -0,0 +1,15 @@
+public class FindMissingNumber {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 5};
+
+        int n = 5;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int number : numbers) {
+            actualSum += number;
+        }
+
+        System.out.println("Missing Number = " + (expectedSum - actualSum));
+    }
+}
