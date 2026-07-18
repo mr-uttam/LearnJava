ed6e15ab0f45456b50729f41c9f11dd4da74bfb3
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-18 09:08:20 +0530

    Add Inverted Hollow Pyramid Java program

---
+++ Q126-Inverted_Hollow_Pyramid.java
@@ -0,0 +1,21 @@
+public class InvertedHollowPyramid {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                if (k == 1 || k == (2 * i - 1) || i == rows) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
