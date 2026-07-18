d6683c77517fd2316c0f51ae2a40e63406035a2d
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-18 09:14:34 +0530

    Add ZigZagPattern class to print zig-zag pattern

---
+++ Q137-Zig_Zag_Pattern.java
@@ -0,0 +1,17 @@
+public class ZigZagPattern {
+    public static void main(String[] args) {
+        int rows = 3;
+        int columns = 9;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
