852a1f95ce4a4a344d0f99d42600873285d16831
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-18 09:29:18 +0530

    Add DecimalToHexadecimal class for conversion

---
+++ Q160-Decimal_to_Hexadecimal.java
@@ -0,0 +1,7 @@
+public class DecimalToHexadecimal {
+    public static void main(String[] args) {
+        int number = 255;
+
+        System.out.println(Integer.toHexString(number).toUpperCase());
+    }
+}
