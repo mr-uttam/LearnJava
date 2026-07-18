9c8937316d9a0b22501fb8fccfaa038c95baa848
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-07-18 09:10:25 +0530

    Add NumberPyramid class to print number pyramid

---
+++ Q130-Number_Pyramid.java
@@ -0,0 +1,17 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
