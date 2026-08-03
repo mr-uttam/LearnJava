a75f6b193ecc2321d0daa7b5d324ed4a23491865
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:43:58 +0530

    Add MethodInJava class with display method

---
+++ R203-Method_in_Java.java
@@ -0,0 +1,11 @@
+public class MethodInJava {
+
+    void display() {
+        System.out.println("Welcome to Java");
+    }
+
+    public static void main(String[] args) {
+        MethodInJava obj = new MethodInJava();
+        obj.display();
+    }
+}
