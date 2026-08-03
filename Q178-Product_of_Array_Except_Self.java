3a8b24c112f2bd34d51afdbb1ac5f4bcb81d37c8
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 10:29:45 +0530

    Implement product of array except self algorithm

---
+++ Q178-Product_of_Array_Except_Self.java
@@ -0,0 +1,25 @@
+public class ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4};
+
+        int n = numbers.length;
+        int[] result = new int[n];
+
+        result[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * numbers[i - 1];
+        }
+
+        int right = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= numbers[i];
+        }
+
+        for (int value : result) {
+            System.out.print(value + " ");
+        }
+    }
+}
