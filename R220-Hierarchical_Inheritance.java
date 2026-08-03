4afb4bdc69878291be41b76a85c9f2fcffb6c1e4
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:52:03 +0530

    Implement hierarchical inheritance with Animal class

---
+++ R220-Hierarchical_Inheritance.java
@@ -0,0 +1,34 @@
+class Animal {
+
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class Cat extends Animal {
+
+    void meow() {
+        System.out.println("Meowing");
+    }
+}
+
+public class HierarchicalInheritance {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.eat();
+        dog.bark();
+
+        cat.eat();
+        cat.meow();
+    }
+}
