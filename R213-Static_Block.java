0d98801ce49844b14dec69b185a722d87e4edff3
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:48:54 +0530

    Add StaticBlock class with static initialization

---
+++ R213-Static_Block.java
@@ -0,0 +1,10 @@
+public class StaticBlock {
+
+    static {
+        System.out.println("Static Block Executed");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+}
