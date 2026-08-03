aea4b7c7a8a0d78a985f8680ebfb575142f481b2
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 12:28:07 +0530

    Create R250-Autoboxing_and_Unboxing.java

---
+++ R250-Autoboxing_and_Unboxing.java
@@ -0,0 +1,12 @@
+public class AutoboxingAndUnboxing {
+
+    public static void main(String[] args) {
+
+        Integer number = 100;
+
+        int value = number;
+
+        System.out.println(number);
+        System.out.println(value);
+    }
+}
