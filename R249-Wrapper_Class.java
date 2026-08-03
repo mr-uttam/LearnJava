9827ac29264553975f337e33ca878e150fc1275a
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 12:26:32 +0530

    Create R249-Wrapper_Class.java

---
+++ R249-Wrapper_Class.java
@@ -0,0 +1,13 @@
+public class WrapperClass {
+
+    public static void main(String[] args) {
+
+        Integer number = Integer.valueOf(100);
+        Double price = Double.valueOf(99.99);
+        Character grade = Character.valueOf('A');
+
+        System.out.println(number);
+        System.out.println(price);
+        System.out.println(grade);
+    }
+}
