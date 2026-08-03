1e809f93894fdc1263622a4045bff5ad923f5ce8
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:45:25 +0530

    Add DefaultConstructor class with default constructor

---
+++ R206-Default_Constructor.java
@@ -0,0 +1,10 @@
+public class DefaultConstructor {
+
+    DefaultConstructor() {
+        System.out.println("Default Constructor Called");
+    }
+
+    public static void main(String[] args) {
+        new DefaultConstructor();
+    }
+}
