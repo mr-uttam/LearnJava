151fb8ba12ef2c25811d86268347696546e778b2
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:46:48 +0530

    Implement method overloading for addition

---
+++ R209-Method_Overloading.java
@@ -0,0 +1,17 @@
+public class MethodOverloading {
+
+    void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    void add(double a, double b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        MethodOverloading obj = new MethodOverloading();
+
+        obj.add(10, 20);
+        obj.add(10.5, 20.5);
+    }
+}
