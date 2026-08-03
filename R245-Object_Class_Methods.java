f77843bd11a81c04c9c5ec52e3683eea8f42b45a
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 12:23:16 +0530

    Add ObjectClassMethods with basic methods demonstration

---
+++ R245-Object_Class_Methods.java
@@ -0,0 +1,11 @@
+public class ObjectClassMethods {
+
+    public static void main(String[] args) {
+
+        ObjectClassMethods obj = new ObjectClassMethods();
+
+        System.out.println(obj.getClass().getName());
+        System.out.println(obj.hashCode());
+        System.out.println(obj.toString());
+    }
+}
