574d625e0d26d4a5e50261ae8bea04f8920a6c07
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:48:27 +0530

    Create R212-Static_Method.java

---
+++ R212-Static_Method.java
@@ -0,0 +1,10 @@
+public class StaticMethod {
+
+    static void display() {
+        System.out.println("Static Method Called");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
