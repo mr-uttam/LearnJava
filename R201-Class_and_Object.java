0a011995bee13396f93bd925f58b43bb6aacc055
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2026-08-03 11:42:55 +0530

    Create R201-Class_and_Object.java

---
+++ R201-Class_and_Object.java
@@ -0,0 +1,11 @@
+public class ClassAndObject {
+    int id = 101;
+    String name = "Uttam";
+
+    public static void main(String[] args) {
+        ClassAndObject student = new ClassAndObject();
+
+        System.out.println(student.id);
+        System.out.println(student.name);
+    }
+}
